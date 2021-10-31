a70a7280016c8ac15513f23b69853765819116de
Mohammad Zafar <mzcoder7@gmail.com>
2021-10-31 16:07:05 +0500

    Sequelize info (index.js) models folder

--- models/index.js
+++ models/index.js
@@ -1,13 +1,31 @@
 const User = require('./User');
 const Project = require('./Project');
+const Comment = require('./Comment');
 
-User.hasMany(Project, {
+User.hasMany(Post, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+  onDelete: 'CASCADE',
 });
 
 Project.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
 });
 
-module.exports = { User, Project };
+Project.hasMany(Comment, {
+  foreignKey: 'post_id',
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+module.exports = { User, Project, Comment };
