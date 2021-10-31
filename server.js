180294a828c93438a3b8a3e06eb6e105aa08bbe8
Mohammad Zafar <mzcoder7@gmail.com>
2021-10-31 16:30:40 +0500

    Mysql2 and jaws in server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const mysql = require('mysql2');
+const connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -20,8 +22,8 @@ const sess = {
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
